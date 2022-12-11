bacfd61c4607b0084f5670c53e84d2815de59f7d
Kalvin Albuquerque <kalvinalbuquerque5@gmail.com>
2022-12-11 17:01:21 -0300

    Exercicio2 CMake Kalvin

--- hellomake.c
+++ hellomake.c
@@ -3,6 +3,7 @@
 int main() {
   // call a function in another file
   myPrintHelloMake();
+  ola();
 
   return(0);
 }
---
+++ subdiretorio/ola.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include <hellomake.h>
+
+void ola(){
+
+printf("Isso eh um teste\n");
+}
